28ef084513b81d98e5a5be59c06a02eb122e8c67
Phil-vP <68397038+Phil-vP@users.noreply.github.com>
2021-02-01 22:00:11 +0100

    30 second split done

--- 30sec_split/split.py
+++ 30sec_split/split.py
@@ -5,6 +5,9 @@ Created on Fri Jan 29 18:30:45 2021
 @author: Philipp
 """
 
+import datetime
+import glob
+
 
 def readFile(pathname):
     
@@ -14,13 +17,64 @@ def readFile(pathname):
     
     for l in allLines:
         print(l)
-    # fout = open("output.txt", "w+")
-    # fout.write("Moinsen")
-    # fout.close()
+    
+    
+    totalDuration = int(allLines[0])
+    startTime = allLines[1]
+    endTime = allLines[2]
+    timestampLines = allLines[4:]
+    
+    offset = 0
+    
+    allTimes = []
+    
+    for l in timestampLines:
+        split = l.split(',')
+        totalSeconds = int(split[2]) - offset
+        appendCount = int(totalSeconds/30)
+        rest = totalSeconds % 30
+        
+        if offset > 15:
+            appendCount += 1
+        
+        if rest >= 15:
+            appendCount += 1
+        
+        extendList = split[3] * appendCount
+        print("Extended the list by " + split[3] + ", " + str(appendCount) + " times")
+        print("Offset? " + str(offset))
+        print("Rest? " + str(rest) + "\n")
+        
+        offset = (30-rest)
+        allTimes.extend(extendList)
+        
+    
+    
+    print(allTimes)
+    
+    print(len(allTimes))
+    
+    date = datetime.datetime.strptime(startTime, '%H:%M')
+    
+    fout = open(''.join(pathname.split(".")[:-1]) + ".csv", "w+")
+    
+    for i in range(len(allTimes)):
+        string = str(date.time()) + " , " + allTimes[i] + "\n"
+        date += datetime.timedelta(seconds=30)
+        
+        fout.write(string)
+    
+    
+    
+    fout.close()
 
 
 def readAllFiles():
-    readFile("B:\\Projekte\\Schlaf\\Auswertung\\VP01_N1.txt")
+    allFiles = glob.glob("B:\\Projekte\\Schlaf\\Auswertung\\*txt")
+    
+    for file in allFiles:
+        readFile(file)
+    
 
 if __name__== "__main__":
     readAllFiles()
